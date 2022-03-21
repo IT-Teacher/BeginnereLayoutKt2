e74a0c35fb0d853eb7795f693c2c83fc3c2d6fef
Admin <mirgiyozom@gmail.com>
2022-03-21 15:24:42 +0500

    add task3

--- app/src/main/java/com/example/beginnerelayoutkt2/MainActivity.kt
+++ app/src/main/java/com/example/beginnerelayoutkt2/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.beginnerelayoutkt2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login = findViewById<EditText>(R.id.login)
+        val parol = findViewById<EditText>(R.id.pw)
+        val res = findViewById<TextView>(R.id.res)
+        val signin = findViewById<Button>(R.id.signin)
+
+        signin.setOnClickListener { view: View? ->
+            res.text = "Login: ${login.text}\nParol: ${parol.text}"
+        }
     }
 }
